3aa55636be76e3f628217da5a8f64e0454c817c2
diligent-utility-1811 <chrisogilvy@cox.net>
2019-02-09 23:02:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return console.log(string.toUpperCase())
+}
+logShout('hello')
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
+logWhisper('hello')
+
