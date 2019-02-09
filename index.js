258a44f909915e8d78cbc14d79fc1bf1935f6c7d
diligent-utility-1811 <chrisogilvy@cox.net>
2019-02-09 23:10:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
     return "I can't hear you!";
   else if (string === "I love you, Grandma.")
-    return "I love you too."
+    return "I love you, too.";
+  else (string === string.toUpperCase())
+    return ""
 }
